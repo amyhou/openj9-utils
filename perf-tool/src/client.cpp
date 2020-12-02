b7ec4602bd7cf61763660051f3a4cff2e352b2f1
Anas Mohamed <amohamed@ualberta.ca>
2020-12-01 23:54:11 -0700

    Refactored client into a class
    Signed-off-by: Anas Mohamed <amohamed@ualberta.ca>

--- perf-tool/src/client.cpp
+++ perf-tool/src/client.cpp
@@ -1,3 +1,5 @@
+#include "client.hpp"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,39 +8,35 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+
 #include "utils.hpp"
-#include <string>
-#include <poll.h>
 
 #define POLL_INTERVAL 150
+#define BUFFER_SIZE 4096
 
-int main(int argc, char *argv[])
+using namespace std;
+
+Client::Client(int _portno, const string _hostname, bool _interactive_mode)
 {
-    int sockfd, portno, n;
-    bool done = false;
+    portno = _portno;
+    hostname = _hostname;
+    interactive_mode = _interactive_mode;
+}
+
+void Client::openServerConnection()
+{
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    struct pollfd pollFds[2];
-    char buffer[4096];
 
-    pollFds[0].fd = STDIN_FILENO;
-    pollFds[0].events = POLLIN;
-    pollFds[0].revents = 0;
-
-    if (argc < 3)
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd < 0)
     {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-    
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
         error("ERROR opening socket");
     }
 
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
+    server = gethostbyname(hostname.c_str());
+    if (server == NULL)
+    {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
@@ -51,52 +49,105 @@ int main(int argc, char *argv[])
           server->h_length);
 
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(socketFd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         error("ERROR connecting");
     }
+}
 
-    pollFds[1].fd = sockfd;
+void Client::startClient()
+{
+
+    openServerConnection();
+
+    if (interactive_mode)
+    {
+        pollFds[0].fd = STDIN_FILENO;
+        pollFds[0].events = POLLIN;
+        pollFds[0].revents = 0;
+        printf("Enter message to send to server: \n");
+    }
+
+    pollFds[1].fd = socketFd;
     pollFds[1].events = POLLIN;
     pollFds[1].revents = 0;
 
-    printf("Enter message to send to server: \n");
+    handlePolling();
+}
 
-    while (!done) {
-        bzero(buffer, 4096);
+void Client::handlePolling()
+{
+    int n;
+    char buffer[BUFFER_SIZE];
 
-        if (poll(pollFds, 2, POLL_INTERVAL) == -1){
+    while (keepPolling)
+    {
+        bzero(buffer, BUFFER_SIZE);
+
+        if (poll(pollFds, 2, POLL_INTERVAL) == -1)
+        {
             error("ERROR on polling");
         }
 
-        if (pollFds[0].revents && POLLIN) {
-            fgets(buffer, 4096, stdin);
+        if (interactive_mode && pollFds[0].revents && POLLIN)
+        {
+            fgets(buffer, BUFFER_SIZE, stdin);
 
-            n = write(sockfd, buffer, strlen(buffer));
-            if (n < 0){ 
+            n = write(socketFd, buffer, strlen(buffer));
+            if (n < 0)
+            {
                 error("ERROR writing to stdin");
             }
         }
 
-        if (pollFds[1].revents && POLLIN) {
-            bzero(buffer, 4096);
+        if (pollFds[1].revents && POLLIN)
+        {
+            bzero(buffer, BUFFER_SIZE);
 
-            n = recv(sockfd, buffer, 4095, MSG_DONTWAIT);
-            if (n < 0) {
+            n = recv(socketFd, buffer, BUFFER_SIZE-1, MSG_DONTWAIT);
+            if (n < 0)
+            {
                 error("ERROR reading from socket");
             }
 
-            if (n > 0) {
+            if (n > 0)
+            {
                 printf("Recieved: %s\n", buffer);
 
-                if (std::string(buffer).substr(strlen(buffer) - 4, 4) == "done") {
-                    done = true;
+                if (string(buffer).substr(strlen(buffer) - 4, 4) == "done")
+                {
+                    keepPolling = false;
                 }
             }
         }
     }
-    
-    close(sockfd);
+
+    closeClient();
+}
+
+void Client::closeClient()
+{
+    keepPolling = false;
+    close(socketFd);
     printf("Closed connection with server.\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    string hostname = "localhost";
+    int portno = 9003;
+
+    if (argc > 2)
+    {
+        hostname = argv[1];
+        if (argc > 3) {
+            portno = atoi(argv[2]);
+        }
+    }
+
+    Client client(portno, hostname, true);
+
+    client.startClient();
 
     return 0;
 }
